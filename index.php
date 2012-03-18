c4ff56962740c0fd79ea53815d7f1e0a0933cf9d
Abimanyu <comelizer@gmail.com>
2012-03-18 22:37:00 +0700

    base file

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/*
++----------------------------------------------------------------------+
+|        INDOBIT-TECHNOLOGIES
+|	 	 based 		: 02-04-2005
+|		 continue 	: December 2011
+|
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|
+|		 Rosi Abimanyu Yusuf	(bima@abimanyu.net) | Pontianak, INDONESIA
+|        (c)2005 INDOBIT.COM | http://www.indobit.com
++----------------------------------------------------------------------+
+*/
+
+@include ("inc/qapuas.php");
+?>
+<script type='text/javascript'>document.location.href='<?=c_ADMINDIR;?>'</script>
